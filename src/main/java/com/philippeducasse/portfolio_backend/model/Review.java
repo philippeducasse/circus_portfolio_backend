0dce1a4dd58745cdc0c3eea0d456ae1df36d6ca6
Philippe Ducasse <ducassephi@hotmail.fr>
2024-08-22 17:22:59 +0200

    initialised repo and created review class

---
+++ src/main/java/com/philippeducasse/portfolio_backend/model/Review.java
@@ -0,0 +1,68 @@
+package com.philippeducasse.portfolio_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Column;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private String author;
+
+    public Review(String content, LocalDateTime createdAt, String author) {
+        this.content = content;
+        this.createdAt = createdAt;
+        this.author = author;
+    }
+
+    public int getId(){
+        return id;
+    };
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setUser(String author) {
+        this.author = author;
+    }
+
+}
