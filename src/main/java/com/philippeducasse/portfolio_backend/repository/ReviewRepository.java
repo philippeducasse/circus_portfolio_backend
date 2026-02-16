6567f71f885af2f1d4b222af322b414d437f5431
philippeducasse <ducassephi@hotmail.fr>
2026-02-16 21:02:48 +0100

    nuke: restarted entire project

--- src/main/java/com/philippeducasse/portfolio_backend/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.philippeducasse.portfolio_backend.repository;
-
-
-import com.philippeducasse.portfolio_backend.model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//extending JpaRepository gives access to save(), findAll(), etc.
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-}
