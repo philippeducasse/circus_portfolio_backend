6f499a2d817c4d8573d860a99162ff395078654f
philippeducasse <ducassephi@hotmail.fr>
2026-02-17 21:13:16 +0100

    feat: added actual endpoints

--- index.php
+++ index.php
@@ -1,19 +1,50 @@
 <?php
+require "db/db.php";
+
+$allowed_origins = ['http://localhost:3000', 'https://philippeducasse.com'];
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if (in_array($origin, $allowed_origins)) {
+    header("Access-Control-Allow-Origin: $origin");
+}
 header("Content-Type: application/json");
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 
 $method = $_SERVER["REQUEST_METHOD"];
 
 if ($method === "POST") {
-    $data = json_decode(file_get_contents("php://input"), true);
+    try {
+        $data = json_decode(file_get_contents("php://input"), true);
 
-    $comment = [
-        "name" => $data["name"] ?? "Anonymous",
-        "message" => $data["message"],
-        "date" => date("d-m-y H:i:s")
-    ];
+        if (!$data) {
+            echo json_encode(['error' => 'Invalid JSON']);
+            exit;
+        }
 
-    echo json_decode($comment["message"]);
+        $comment = [
+            "name" => $data["name"] ?? "Anonymous",
+            "show" => $data["show"] ?? null,
+            "message" => $data["message"],
+            "date" => date("d-m-y H:i:s")
+        ];
+
+        // SQL prepare statement protects against SQL injection attacks
+        $stmt = $db->prepare("INSERT INTO comments (name, show, message, date) VALUES (:name, :show, :message, :date)");
+        $stmt->execute($comment);
+
+
+        echo json_encode(["success" => true, "id" => $db->lastInsertId()]);
+    } catch (Exception $e) {
+        http_response_code(400);
+        echo json_encode(['error' => $e->getMessage()]);
+    }
+}
+if ($method === "GET") {
+    $stmt = $db->prepare("SELECT * FROM comments ORDER BY date DESC");
+    $stmt->execute();
+    // returns associative arrays instead of objects. They are simpler and JSON encode handles them naturally
+    echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
 } else {
-
-    echo json_encode(["message" => "Hello World!"]);
+    echo json_encode(["success" => false]);
 }
